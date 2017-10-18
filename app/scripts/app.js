f32687fe952991053cdad30bf84a840e34f4cb55
Mark Strizalkouski <markstrizalkouski@gmail.com>
2017-10-18 12:28:16 -0400

    fixed some syntax issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,16 +8,19 @@
     $stateProvider
       .state('landing', {
         url: '/',
-        templateUrl: '/templates.landing.html'
+        templateUrl: '/templates/landing.html'
       })
-      .state('album',
-        url: '/album'
+      .state('album', {
+        url: '/album',
         templateUrl: '/templates/album.html'
       })
       .state('collection',{
-        url: '/collection'
-        templateUrl: '/templates.collection.html'
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
+    
+  }
+  
   angular
     .module('blocJams', ['ui.router'])
     .config(config);
