108b9224e07f50f308713e587dc573610d798b7a
Mark Strizalkouski <markstrizalkouski@gmail.com>
2017-10-30 13:29:41 -0400

    progress in checkpoint 7, music isn't playing and play/pause buttons are not swapping

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,50 @@
+ (function() {
+     function SongPlayer() {
+          var SongPlayer = {};
+          var currentSong = null;
+          /**
+          *@desc Buzz object audio file
+          *@type {Object}
+          */
+          var currentBuzzObject = null;
+      SongPlayer.play = function(song) {
+         if (currentSong !== song){
+          setSong(song);
+         currentBuzzObject.play();
+       }
+        else if (currentSong === song){
+          if (currentBuzzObject.isPaused()){
+            currentBuzzObject.play();
+            song.playing = true;
+          }
+        }
+    };
+       SongPlayer.pause = function(song){
+         currentBuzzObject.pause();
+         song.playing = false;
+       };
+       /**
+       *@function setSong
+       *@desc Stops currently playing song and loads new audio file as currentBuzzObject
+       *@PARAM {Object} song
+       */
+       var setSong = function(song) {
+          if (currentBuzzObject) {
+            currentBuzzObject.stop();
+            currentSong.playing = null;
+          }
+ 
+        currentBuzzObject = new buzz.sound(song.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+ 
+    currentSong = song;
+ };
+          return SongPlayer;
+}
+ 
+     angular
+         .module('blocJams')
+         .factory('SongPlayer', SongPlayer);
+ })();
